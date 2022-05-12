eb107b52bebbc74f1c66b02261449069f8a6fc32
leedavidcscba <leedavidcuellar@gmail.com>
2022-05-11 21:54:58 -0300

    Inicio Proyecto

---
+++ src/test/java/com/proyectofinal/tukiwallet/TukiWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.tukiwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukiWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
